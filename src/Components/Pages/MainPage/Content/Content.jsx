b74bea23a323efecb9db6093551baff73f931535
Frontovichok <magomed.1996.k@gmail.com>
2021-04-20 14:13:59 +0300

    added ourTeam block

--- src/Components/Pages/MainPage/Content/Content.jsx
+++ src/Components/Pages/MainPage/Content/Content.jsx
@@ -2,16 +2,18 @@ import CalculatePrice from "./CalculatePrice/CalculatePrice";
 import Carousel from "./Carousel/Carousel";
 import styles from "./Content.module.css";
 import OurProducts from "./OurProducts/OutProducts";
+import OurTeam from "./OurTeam/OurTeam";
 import Services from "./Services/Services";
 import WhyWe from "./WhyWe/WhyWe";
 
 function Content() {
   return (
     <>
-        <Carousel />
-        <Services />
-        <OurProducts />
-        <WhyWe/>
+      <Carousel />
+      <Services />
+      <OurProducts />
+      <WhyWe />
+      <OurTeam />
     </>
   );
 }
