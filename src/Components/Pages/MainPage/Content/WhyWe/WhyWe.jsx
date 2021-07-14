5c91623afef4ef12cd8a03a8d8099a0aaf6a952f
Frontovichok <magomed.1996.k@gmail.com>
2021-07-14 15:45:21 +0300

    added animation of increased number to why we block in main page

--- src/Components/Pages/MainPage/Content/WhyWe/WhyWe.jsx
+++ src/Components/Pages/MainPage/Content/WhyWe/WhyWe.jsx
@@ -4,9 +4,30 @@ import rating from "./rating.svg";
 import calendar from "./calendar.svg";
 import term from "./term.svg";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/opacity.css";
+// import "react-lazy-load-image-component/src/effects/opacity.css";
+import CountUp from "react-countup";
+import VisibilitySensor from "react-visibility-sensor";
+import { useState } from "react";
 
 function WhyWe() {
+  let [endedNumbersAnimation, endNumbersAnimation] = useState([
+    false,
+    false,
+    false,
+    false,
+  ]);
+
+  const numbersAnimationOnChange = (isVisible, start, animantionNumber) => {
+    if (endedNumbersAnimation[animantionNumber] === false && isVisible) {
+      endNumbersAnimation(
+        endedNumbersAnimation.map((numb, i) =>
+          i === animantionNumber ? (numb = true) : numb
+        )
+      );
+      start();
+    }
+  };
+
   return (
     <section className={`sectionMainPage ${styles.whyWe}`}>
       <div className={styles.imageContainer}>
@@ -18,28 +39,55 @@ function WhyWe() {
         <div className={styles.advantagesContainer}>
           <div className={styles.advantageCard}>
             {/* <img src={hammer} className={styles.advantageImage} alt="Why We" /> */}
-            <LazyLoadImage
+            {/* <LazyLoadImage
               className={styles.advantageImage}
               alt="Why We"
               effect="opacity"
               src={hammer}
-            />
+            /> */}
             <div>
-              <p className={styles.number}>1500 м²</p>
+              <p className={styles.number}>
+                <CountUp end={1500} duration={2} redraw={true}>
+                  {({ countUpRef, start }) => (
+                    <VisibilitySensor
+                      onChange={(isVisible) => {
+                        numbersAnimationOnChange(isVisible, start, 1);
+                      }}
+                      delayedCall
+                    >
+                      <span ref={countUpRef} />
+                    </VisibilitySensor>
+                  )}
+                </CountUp>{" "}
+                м²
+              </p>
               <p className={styles.title}>Смонтировано</p>
             </div>
           </div>
           <div className={styles.advantageCard}>
             {/* <img src={rating} className={styles.advantageImage} alt="Why We" />
              */}
-            <LazyLoadImage
+            {/* <LazyLoadImage
               className={styles.advantageImage}
               alt="Why We"
               effect="opacity"
               src={rating}
-            />
+            /> */}
             <div>
-              <p className={styles.number}>15</p>
+              <p className={styles.number}>
+                <CountUp end={15} redraw={false} duration={1} redraw={true}>
+                  {({ countUpRef, start }) => (
+                    <VisibilitySensor
+                      onChange={(isVisible) => {
+                        numbersAnimationOnChange(isVisible, start, 2);
+                      }}
+                      delayedCall
+                    >
+                      <span ref={countUpRef} />
+                    </VisibilitySensor>
+                  )}
+                </CountUp>
+              </p>
               <p className={styles.title}>Выполненных проектов</p>
             </div>
           </div>
@@ -49,28 +97,54 @@ function WhyWe() {
               className={styles.advantageImage}
               alt="Why We"
             /> */}
-            <LazyLoadImage
+            {/* <LazyLoadImage
               className={styles.advantageImage}
               alt="Why We"
               effect="opacity"
               src={calendar}
-            />
+            /> */}
             <div>
-              <p className={styles.number}>5</p>
+              <p className={styles.number}>
+                <CountUp end={5} duration={2} redraw={true}>
+                  {({ countUpRef, start }) => (
+                    <VisibilitySensor
+                      onChange={(isVisible) => {
+                        numbersAnimationOnChange(isVisible, start, 3);
+                      }}
+                      delayedCall
+                    >
+                      <span ref={countUpRef} />
+                    </VisibilitySensor>
+                  )}
+                </CountUp>
+              </p>
               <p className={styles.title}>Лет на рынке</p>
             </div>
           </div>
           <div className={styles.advantageCard}>
             {/* <img src={term} className={styles.advantageImage} alt="Why We" /> */}
-            <LazyLoadImage
+            {/* <LazyLoadImage
               className={styles.advantageImage}
               alt="Why We"
               effect="opacity"
               src={term}
-            />
+            /> */}
             <div>
-              <p className={styles.number}>2</p>
-              <p className={styles.title}>Года гарантии на работу</p>
+              <p className={styles.number}>
+                <CountUp end={10} duration={2} redraw={true}>
+                  {({ countUpRef, start }) => (
+                    <VisibilitySensor
+                      onChange={(isVisible) => {
+                        numbersAnimationOnChange(isVisible, start, 4);
+                      }}
+                      delayedCall
+                    >
+                      <span ref={countUpRef} />
+                    </VisibilitySensor>
+                  )}
+                </CountUp>
+              </p>
+              <p className={styles.title}>Лет гарантии на работу</p>
             </div>
           </div>
         </div>
